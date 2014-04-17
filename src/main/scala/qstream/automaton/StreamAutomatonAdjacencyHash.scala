3b043d32c445881161b5df3e90c545589125f154
qdx <qdxzzz@gmail.com>
2014-04-17 14:42:42 -0700

    add find path method

---
+++ src/main/scala/qstream/automaton/StreamAutomatonAdjacencyHash.scala
@@ -0,0 +1,29 @@
+package qstream.automaton
+
+import scala.collection.mutable
+
+class StreamAutomatonAdjacencyHash extends StreamAutomaton{
+
+  var current_state = 0
+  var earliest_state = 0
+  var stream_count = 0
+
+  //                                  [state, hash(seq_num, state)]
+  var automaton = new mutable.HashMap[Int, mutable.HashMap[Int, State]]()
+  automaton(0) = new mutable.HashMap[Int, State]()
+
+  def input(transfer: State): Unit = {
+
+
+  }
+
+  def find_state(state: Int): mutable.Queue[(Int, State)] = {
+
+    new mutable.Queue[(Int, State)]()
+  }
+
+  def show(): Unit = {
+
+  }
+
+}
