fc4f89eecae1cbda0c81b8b25da9e73dd99c53c8
qdx <qdxzzz@gmail.com>
2014-04-18 14:39:58 -0700

    added walk path

--- src/main/scala/qstream/automaton/StreamAutomatonAdjacencyList.scala
+++ src/main/scala/qstream/automaton/StreamAutomatonAdjacencyList.scala
@@ -14,13 +14,17 @@ object StreamAutomatonAdjacencyList extends App {
   x.show()
   x.print_path()
   println(x.find_state(3).mkString)
-  println(y.find_state(3).mkString)
+  println(x.find_state(5).mkString)
+  //  println(y.find_state(3).mkString)
+  println(x.find_paths(3, 5))
+  println(x.find_paths(3, 3))
+  println(x.walk_path(7,10).mkString(" -> "))
   //y.show()
 }
 
 // To query state with time, build an index of states based on time
 // interval that makes traversal fast enough
-class StreamAutomatonAdjacencyList extends StreamAutomaton{
+class StreamAutomatonAdjacencyList extends StreamAutomaton {
   var current_state = 0
   var earliest_state = 0
   var stream_count = 0
@@ -41,6 +45,60 @@ class StreamAutomatonAdjacencyList extends StreamAutomaton{
   def find_state(state: Int): mutable.Queue[(Int, State)] = {
     var result = new mutable.Queue[(Int, State)]
     result ++= automaton(state)
+  }
+
+  def find_paths(state_src: Int, state_dst: Int, length: Int = -1): mutable.Queue[(Int, Int)] = {
+    var src_i = 0
+    var dst_i = 0
+    val src_out_degree = automaton(state_src).length
+    val dst_out_degree = automaton(state_dst).length
+    val result = new mutable.Queue[(Int, Int)]
+    while (src_i < src_out_degree && dst_i < dst_out_degree) {
+      val seq_src = automaton(state_src)(src_i)._1
+      val seq_dst = automaton(state_dst)(dst_i)._1
+      if (seq_src < seq_dst) {
+        if ((src_i + 1 < src_out_degree) && (automaton(state_src)(src_i + 1)._1 < seq_dst)) {
+          src_i += 1
+        }
+        else {
+          result.enqueue((seq_src, seq_dst))
+          src_i += 1
+          dst_i += 1
+        }
+      } else {
+        dst_i += 1
+      }
+    }
+    result
+  }
+
+  // ATTENTION: did not handle cases where length exceeds maximum value
+  def walk_path(seq: Int, length: Int, state: Int = -1): Array[Int] = {
+    var start = (-1, -1)
+    if (state == -1) start = find_by_seq(seq)
+    else start = (state, binary_find(seq, automaton(state)))
+    var result = new Array[Int](0)
+    var c_state = start._1
+    var c_key = start._2
+    var c_seq = seq
+    for (i <- Range(0, length)) {
+      result = result :+ c_state
+      c_seq += 1
+      c_state = automaton(c_state)(c_key)._2.state
+      c_key = binary_find(c_seq, automaton(c_state))
+    }
+    result
+  }
+
+  //(state, index)
+  def find_by_seq(seq: Int): (Int, Int) = {
+    for ((k, v) <- automaton) {
+      val key = binary_find(seq, v)
+      if (key != -1 && v(key)._1 == seq) {
+        return (k, key)
+      }
+    }
+    (-1, -1)
   }
 
   // return the index of the key, or the index of the smallest element that is greater than key
