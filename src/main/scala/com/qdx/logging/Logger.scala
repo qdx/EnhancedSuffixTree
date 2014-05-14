5f18d431e7b05d53aed89a5a7ba6f3d81a1afe49
qdx <qdxzzz@gmail.com>
2014-05-13 22:28:14 -0700

    worte my logger

---
+++ src/main/scala/com/qdx/logging/Logger.scala
@@ -0,0 +1,28 @@
+package com.qdx.logging
+
+object Logger {
+  val ERROR = 0
+  val WARN = 1
+  val DEBUG = 2
+  val INFO = 3
+}
+
+trait Logger {
+  var log_level = Logger.DEBUG
+
+  def debug(s: String): Unit  =  if(log_level >= Logger.DEBUG) log(s)
+  def info(s: String): Unit  =  if(log_level >= Logger.INFO) log(s)
+  def warn(s: String): Unit  =  if(log_level >= Logger.WARN) log(s)
+  def error(s: String): Unit  =  if(log_level >= Logger.ERROR) log(s)
+
+  private def log(s: String): Unit = {
+    val log_head = log_level match {
+      case Logger.INFO => "[INFO]: "
+      case Logger.WARN => "[WARN]: "
+      case Logger.ERROR => "[ERROR]: "
+      case Logger.DEBUG => "[DEBUG]: "
+    }
+    println(log_head + s)
+  }
+
+}
