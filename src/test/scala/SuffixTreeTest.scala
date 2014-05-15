904bdd693487ba0e7823a53e65f4b8225bf642a1
qdx <qdxzzz@gmail.com>
2014-05-14 18:03:52 -0700

    test for suffix tree which tests whether the suffix has correct number of terminal nodes

---
+++ src/test/scala/SuffixTreeTest.scala
@@ -0,0 +1,25 @@
+import com.qdx.suffixtree.SuffixTree
+import com.qdx.suffixtree.Node
+import com.qdx.logging.Logger
+import org.scalacheck.Prop.forAll
+import org.scalacheck._
+
+object SuffixTreeTest extends Properties("Suffix Tree Properties") {
+  val alphabet = Gen.choose('a', 'z')
+  val aTozString = for{
+    size <- Gen.choose(1000, 10000)
+    s <- Gen.listOfN(size, alphabet)
+  } yield s.mkString
+
+  property("numOfLeafNodes") = forAll(aTozString){ s: String =>
+    val stree = new SuffixTree[Char]
+    stree.log_level = Logger.ERROR
+    stree.batch_input(s)
+    stree.insert('#')
+    var leaf_counter = 0
+    for(n <- stree.breadth_first_traverse())
+      if(n.type_ == Node.LEAF_NODE)
+        leaf_counter += 1
+    leaf_counter == s.length + 1
+  }
+}
