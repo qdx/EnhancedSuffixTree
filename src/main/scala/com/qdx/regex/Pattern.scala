036c91dd34a1962c032a2f4639b7537c12b93636
qdx <qdxzzz@gmail.com>
2014-05-20 22:16:40 -0700

    regex to dfa refactored

--- src/main/scala/com/qdx/regex/Pattern.scala
+++ src/main/scala/com/qdx/regex/Pattern.scala
@@ -1,10 +1,11 @@
 package com.qdx.regex
 
 import scala.collection.mutable
-import com.qdx.logging.Logger
 import scala.collection.mutable.ArrayBuffer
+import com.qdx.logging.Logger
 
 object Pattern {
+  // some special characters used to handle inner representation
   val CONCAT = 215.toChar
   val EPSILON = 7.toChar
   val ANY = 6.toChar
@@ -46,207 +47,8 @@ class Pattern(p: String) extends Logger {
     nfa_to_dfa(nfa)
   }
 
-  private def infix_to_postfix(postfix_re: String): String = {
-    val stack = new mutable.Stack[Char]()
-    val result = new mutable.StringBuilder()
-    for (c <- postfix_re) {
-      c match {
-        case '(' => stack.push(c)
-        case ')' =>
-          while (stack.top != '(') result.append(stack.pop())
-          stack.pop()
-        case _ =>
-          while (!stack.isEmpty && Pattern.get_precedence(stack.top) > Pattern.get_precedence(c)) {
-            result.append(stack.pop())
-          }
-          stack.push(c)
-      }
-    }
-    while (stack.nonEmpty) result.append(stack.pop())
-    result.toString()
-  }
-
-  private def add_explicit_concat(re: String): String = {
-    val result = new mutable.StringBuilder()
-    var prev = None: Option[Char]
-    for (c <- re) {
-      val p = prev.getOrElse()
-      if (prev.isEmpty
-        || p == '('
-        || p == '\\'
-        || Pattern.BINARY_OPERATOR.contains(p)
-        || c == ')'
-        || (Pattern.OPERATOR_PRECEDENCE.contains(c) && c != '(' && c != '.' && c != '\\')) {
-        result.append(c)
-      } else {
-        result.append(Pattern.CONCAT + "" + c)
-      }
-      prev = Some(c)
-    }
-    result.toString()
-  }
-
-  private def nfa_to_dfa(nfa: FiniteAutomaton): FiniteAutomaton = {
-    println(show(nfa))
-    val dfa = new FiniteAutomaton
-
-    // prepare the start state
-    val d_to_n_set = new mutable.HashMap[mutable.HashSet[State], State]()
-    val dfa_start = new mutable.HashSet[State]()
-    dfa_start ++= epsilon_closure(Array(nfa.start))
-    dfa_start.add(nfa.start)
-    d_to_n_set(dfa_start) = new State
-    dfa.start = d_to_n_set(dfa_start)
-    assert(d_to_n_set.contains(dfa_start))
-
-    val queue = new mutable.Queue[mutable.HashSet[State]]()
-    queue.enqueue(dfa_start)
-    while (queue.nonEmpty) {
-      val l = queue.length
-      val add_queue = new mutable.Queue[mutable.HashSet[State]]()
-      for (i <- queue) {
-        d_to_n_set(i).accept = i.exists(p => p.accept)
-        debug("queue size:" + queue.length)
-        for (s <- i) {
-          for ((k, v) <- s.to) {
-            if (!d_to_n_set(i).to.contains(k) && k != Pattern.EPSILON) {
-              debug("to: " + k)
-              val to_set = move(k, i)
-              val epsilon_to_set = epsilon_closure(to_set) ++= to_set
-              val hash_epsilon_to_state_set = new mutable.HashSet[State]()
-              hash_epsilon_to_state_set ++= epsilon_to_set
-              if (d_to_n_set.contains(hash_epsilon_to_state_set)) {
-                d_to_n_set(i).add_next(k, d_to_n_set(hash_epsilon_to_state_set))
-              } else {
-                val to_state = new State
-                d_to_n_set(hash_epsilon_to_state_set) = to_state
-                d_to_n_set(i).add_next(k, to_state)
-              }
-              add_queue.enqueue(hash_epsilon_to_state_set)
-            }
-          }
-        }
-        System.in.read()
-      }
-      queue ++= add_queue
-      Range(0, l).foreach(_ => queue.dequeue())
-    }
-    dfa
-  }
-
-  private def move(via: Char, states: Iterable[State]): ArrayBuffer[State] = {
-    val result = new ArrayBuffer[State]()
-    for (s <- states) {
-      if (s.to.contains(via)) {
-        result ++= s.to(via)
-      }
-    }
-    result
-  }
-
-  private def epsilon_closure(states: Iterable[State]): ArrayBuffer[State] = {
-    val stack = new mutable.Stack[State]()
-    val result = new ArrayBuffer[State]()
-    for (s <- states) {
-      stack.push(s)
-    }
-    while (stack.nonEmpty) {
-      val s = stack.pop()
-      if (s.to.contains(Pattern.EPSILON)) {
-        for (t <- s.to(Pattern.EPSILON)) {
-          if (!result.contains(t)) {
-            result.append(t)
-            stack.push(t)
-          }
-        }
-      }
-    }
-    result
-  }
-
-  private def postfix_to_nfa(re: String): FiniteAutomaton = {
-    val stack = new mutable.Stack[FiniteAutomaton]()
-    for (i <- Range(0, re.length)) {
-      if (i < re.length - 1 && re(i + 1) == '\\') {
-        nfa_char(re(i), stack)
-      } else {
-        re(i) match {
-          case '|' => nfa_or(stack)
-          case '?' => nfa_question(stack)
-          case '+' => nfa_plus(stack)
-          case '*' => nfa_star(stack)
-          case '.' => nfa_dot(stack)
-          case '\\' => Unit
-          case Pattern.CONCAT => nfa_concat(stack)
-          case _ =>
-            if (!Pattern.LITERAL_SET.contains(re(i)))
-              error("char not in the literal detected")
-            nfa_char(re(i), stack)
-        }
-      }
-    }
-    stack.pop()
-  }
-
-  private def nfa_char(c: Char, stack: mutable.Stack[FiniteAutomaton]): Unit = {
-    val nfa = new FiniteAutomaton
-    nfa.start.add_next(c, nfa.end)
-    nfa.end.accept = true
-    stack.push(nfa)
-  }
-
-  private def nfa_dot(stack: mutable.Stack[FiniteAutomaton]): Unit = {
-    val nfa = new FiniteAutomaton
-    nfa.start.add_next(Pattern.ANY, nfa.end)
-    nfa.end.accept = true
-    stack.push(nfa)
-  }
-
-  private def nfa_or(stack: mutable.Stack[FiniteAutomaton]): Unit = {
-    val new_nfa = new FiniteAutomaton
-    val nfa1 = stack.pop()
-    val nfa2 = stack.pop()
-    nfa1.end.accept = false
-    nfa2.end.accept = false
-    new_nfa.start.add_next(Pattern.EPSILON, nfa1.start)
-    new_nfa.start.add_next(Pattern.EPSILON, nfa2.start)
-    nfa1.end.add_next(Pattern.EPSILON, new_nfa.end)
-    nfa2.end.add_next(Pattern.EPSILON, new_nfa.end)
-    new_nfa.end.accept = true
-    stack.push(new_nfa)
-  }
-
-  private def nfa_concat(stack: mutable.Stack[FiniteAutomaton]): Unit = {
-    val new_nfa = new FiniteAutomaton
-    val nfa1 = stack.pop()
-    val nfa2 = stack.pop()
-    nfa2.end.accept = false
-    new_nfa.start = nfa2.start
-    new_nfa.end = nfa1.end
-    nfa2.end.add_next(Pattern.EPSILON, nfa1.start)
-    stack.push(new_nfa)
-  }
-
-  private def nfa_question(stack: mutable.Stack[FiniteAutomaton]): Unit = {
-    val nfa = stack.pop()
-    nfa.start.add_next(Pattern.EPSILON, nfa.end)
-    stack.push(nfa)
-  }
-
-  private def nfa_star(stack: mutable.Stack[FiniteAutomaton]): Unit = {
-    val nfa = stack.pop()
-    nfa.end.add_next(Pattern.EPSILON, nfa.start)
-    nfa.start.add_next(Pattern.EPSILON, nfa.end)
-    stack.push(nfa)
-  }
-
-  private def nfa_plus(stack: mutable.Stack[FiniteAutomaton]): Unit = {
-    val nfa = stack.pop()
-    nfa.end.add_next(Pattern.EPSILON, nfa.start)
-    stack.push(nfa)
-  }
-
-  def show(fa: FiniteAutomaton): String = {
+  // print the FA into a dot language string
+  def show(fa: FiniteAutomaton = dfa): String = {
     val id_map = mutable.HashMap[State, Int]()
     val queue = new mutable.Queue[State]()
     queue.enqueue(fa.start)
@@ -254,7 +56,6 @@ class Pattern(p: String) extends Logger {
     val visited = ArrayBuffer[Int]()
     visited.append(0)
 
-    //    val id_map = new mutable.HashMap[State, Int]()
     id_map(fa.start) = 0
 
     var id_counter = 1
@@ -298,4 +99,223 @@ class Pattern(p: String) extends Logger {
     sb.append("}")
     sb.toString()
   }
+
+  private def add_explicit_concat(re: String): String = {
+    val result = new mutable.StringBuilder()
+    var prev = None: Option[Char]
+    for (c <- re) {
+      val p = prev.getOrElse()
+      if (prev.isEmpty
+        || p == '('
+        || p == '\\'
+        || Pattern.BINARY_OPERATOR.contains(p)
+        || c == ')'
+        || (Pattern.OPERATOR_PRECEDENCE.contains(c) && c != '(' && c != '.' && c != '\\')) {
+        result.append(c)
+      } else {
+        result.append(Pattern.CONCAT + "" + c)
+      }
+      prev = Some(c)
+    }
+    result.toString()
+  }
+
+  // standard stack to postfix approach
+  private def infix_to_postfix(postfix_re: String): String = {
+    val stack = new mutable.Stack[Char]()
+    val result = new mutable.StringBuilder()
+    for (c <- postfix_re) {
+      c match {
+        case '(' => stack.push(c)
+        case ')' =>
+          while (stack.top != '(') result.append(stack.pop())
+          stack.pop()
+        case _ =>
+          while (!stack.isEmpty && Pattern.get_precedence(stack.top) > Pattern.get_precedence(c)) {
+            result.append(stack.pop())
+          }
+          stack.push(c)
+      }
+    }
+    while (stack.nonEmpty) result.append(stack.pop())
+    result.toString()
+  }
+
+  // McMaughton-Yamada-Thompson algorithm which transform postfix regex into nfa
+  private def postfix_to_nfa(re: String): FiniteAutomaton = {
+    val stack = new mutable.Stack[FiniteAutomaton]()
+    for (i <- Range(0, re.length)) {
+      if (i < re.length - 1 && re(i + 1) == '\\') {
+        nfa_char(re(i), stack)
+      } else {
+        re(i) match {
+          case '|' => nfa_or(stack)
+          case '?' => nfa_question(stack)
+          case '+' => nfa_plus(stack)
+          case '*' => nfa_star(stack)
+          case '.' => nfa_dot(stack)
+          case '\\' => Unit
+          case Pattern.CONCAT => nfa_concat(stack)
+          case _ =>
+            if (!Pattern.LITERAL_SET.contains(re(i)))
+              error("char not in the literal detected")
+            nfa_char(re(i), stack)
+        }
+      }
+    }
+    stack.pop()
+  }
+
+  // In the following, S is start, E is end, e is epsilon, s_2 is state
+  // S---[c]--->E
+  private def nfa_char(c: Char, stack: mutable.Stack[FiniteAutomaton]): Unit = {
+    val nfa = new FiniteAutomaton
+    nfa.start.add_next(c, nfa.end)
+    nfa.end.accept = true
+    stack.push(nfa)
+  }
+
+  // S---[ANY]--->E
+  private def nfa_dot(stack: mutable.Stack[FiniteAutomaton]): Unit = {
+    val nfa = new FiniteAutomaton
+    nfa.start.add_next(Pattern.ANY, nfa.end)
+    nfa.end.accept = true
+    stack.push(nfa)
+  }
+
+  // S---e--->nfa1---e---->E
+  //  \---e--->nfa2---e-->/
+  private def nfa_or(stack: mutable.Stack[FiniteAutomaton]): Unit = {
+    val new_nfa = new FiniteAutomaton
+    val nfa1 = stack.pop()
+    val nfa2 = stack.pop()
+    nfa1.end.accept = false
+    nfa2.end.accept = false
+    new_nfa.start.add_next(Pattern.EPSILON, nfa1.start)
+    new_nfa.start.add_next(Pattern.EPSILON, nfa2.start)
+    nfa1.end.add_next(Pattern.EPSILON, new_nfa.end)
+    nfa2.end.add_next(Pattern.EPSILON, new_nfa.end)
+    new_nfa.end.accept = true
+    stack.push(new_nfa)
+  }
+
+  // S--->nfa1---e--->nfa2--->E
+  private def nfa_concat(stack: mutable.Stack[FiniteAutomaton]): Unit = {
+    val new_nfa = new FiniteAutomaton
+    val nfa1 = stack.pop()
+    val nfa2 = stack.pop()
+    nfa2.end.accept = false
+    new_nfa.start = nfa2.start
+    new_nfa.end = nfa1.end
+    nfa2.end.add_next(Pattern.EPSILON, nfa1.start)
+    stack.push(new_nfa)
+  }
+
+  // S------e------E
+  //  \--->nfa--->/
+  private def nfa_question(stack: mutable.Stack[FiniteAutomaton]): Unit = {
+    val nfa = stack.pop()
+    nfa.start.add_next(Pattern.EPSILON, nfa.end)
+    stack.push(nfa)
+  }
+
+  //  /<----e-----\
+  // S------e----->E
+  //  \--->nfa--->/
+  private def nfa_star(stack: mutable.Stack[FiniteAutomaton]): Unit = {
+    val nfa = stack.pop()
+    nfa.end.add_next(Pattern.EPSILON, nfa.start)
+    nfa.start.add_next(Pattern.EPSILON, nfa.end)
+    stack.push(nfa)
+  }
+
+  //  /<----e-----\
+  // S---->nfa---->E
+  private def nfa_plus(stack: mutable.Stack[FiniteAutomaton]): Unit = {
+    val nfa = stack.pop()
+    nfa.end.add_next(Pattern.EPSILON, nfa.start)
+    stack.push(nfa)
+  }
+
+  // subset construction algorithm to transform nfa into dfa, which uses
+  // helper method move() and epsilon_closure() below.
+  private def nfa_to_dfa(nfa: FiniteAutomaton): FiniteAutomaton = {
+    println(show(nfa))
+    val dfa = new FiniteAutomaton
+
+    // prepare the start state
+    val d_to_n_set = new mutable.HashMap[mutable.HashSet[State], State]()
+    val dfa_start = new mutable.HashSet[State]()
+    // these nfa states form the start state of the dfa
+    dfa_start ++= epsilon_closure(Array(nfa.start))
+    dfa_start.add(nfa.start)
+    d_to_n_set(dfa_start) = new State
+    dfa.start = d_to_n_set(dfa_start)
+
+    // a BFS-like traverse on the nfa
+    val queue = new mutable.Queue[mutable.HashSet[State]]()
+    queue.enqueue(dfa_start)
+    while (queue.nonEmpty) {
+      val l = queue.length
+      // states that visisted in this loop that need to be added back to the queue
+      val add_queue = new mutable.Queue[mutable.HashSet[State]]()
+      for (i <- queue) {
+        // if there is any state in the nfa states the dfa state represents is an
+        // accepting state, the dfa state is an accepting state
+        d_to_n_set(i).accept = i.exists(p => p.accept)
+        for (s <- i) {
+          for ((k, v) <- s.to) {
+            if (!d_to_n_set(i).to.contains(k) && k != Pattern.EPSILON) {
+              val to_set = move(k, i)
+              val epsilon_to_set = epsilon_closure(to_set) ++= to_set
+              val hash_epsilon_to_state_set = new mutable.HashSet[State]()
+              hash_epsilon_to_state_set ++= epsilon_to_set
+              if (d_to_n_set.contains(hash_epsilon_to_state_set)) {
+                d_to_n_set(i).add_next(k, d_to_n_set(hash_epsilon_to_state_set))
+              } else {
+                val to_state = new State
+                d_to_n_set(hash_epsilon_to_state_set) = to_state
+                d_to_n_set(i).add_next(k, to_state)
+              }
+              add_queue.enqueue(hash_epsilon_to_state_set)
+            }
+          }
+        }
+      }
+      queue ++= add_queue
+      Range(0, l).foreach(_ => queue.dequeue())
+    }
+    dfa
+  }
+
+  private def move(via: Char, states: Iterable[State]): ArrayBuffer[State] = {
+    val result = new ArrayBuffer[State]()
+    for (s <- states) {
+      if (s.to.contains(via)) {
+        result ++= s.to(via)
+      }
+    }
+    result
+  }
+
+  private def epsilon_closure(states: Iterable[State]): ArrayBuffer[State] = {
+    val stack = new mutable.Stack[State]()
+    val result = new ArrayBuffer[State]()
+    for (s <- states) {
+      stack.push(s)
+    }
+    while (stack.nonEmpty) {
+      val s = stack.pop()
+      if (s.to.contains(Pattern.EPSILON)) {
+        for (t <- s.to(Pattern.EPSILON)) {
+          if (!result.contains(t)) {
+            result.append(t)
+            stack.push(t)
+          }
+        }
+      }
+    }
+    result
+  }
+
 }
